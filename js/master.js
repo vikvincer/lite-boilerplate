730e4f3f24248409c628f42f1862dcdeae88ae75
ridonvi@mfcgd.com <ridonvi@mfcgd.com>
2019-08-29 11:37:03 +0800

    initial commit

---
+++ js/master.js
@@ -0,0 +1,22 @@
+
+var script = (function() {
+
+    const index = {
+        nav: ()=> {
+            // alert('nav');
+        },
+        scroll: ()=> {
+            // alert('scroll');
+        }
+    }
+
+
+    function init() {
+        // Initialize functions
+
+    }
+    return {
+        init
+    }
+}());
+jQuery(document).ready(function($) { script.init(); });
